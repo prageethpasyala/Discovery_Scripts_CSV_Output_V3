ecec8e8e2bb05a27f1d158b52e57a88b5c31c112
PK_Hub <93587237+prageethpasyala@users.noreply.github.com>
2024-01-24 00:22:31 +0000

    fixed used services script

---
+++ my_roles_discovery.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Specify the AWS region
+AWS_REGION=$1
+
+# Get list of IAM roles
+role=$(aws iam list-roles --region $AWS_REGION --query "Roles[*].[RoleName,Arn]" --output text)
+
+# Output headers to CSV file
+echo "RoleName,RoleArn,AttachedPolicies" > $2
+
+# Loop through each IAM role and append details to CSV file
+while read -r role; do
+    role_name=$(echo "$role" | cut -f1)
+    role_arn=$(echo "$role" | cut -f2)
+    
+    # Get list of attached policies for each role
+    # attached_policies=$(aws iam list-attached-role-policies --role-name $role_name --region $AWS_REGION --query "AttachedPolicies[*].PolicyName" --output text)
+    attached_policies=$(aws iam list-attached-role-policies --role-name $role_name --region $AWS_REGION --output text)
+    
+    echo "$role_name,$role_arn,\"$attached_policies\"" | tr '\t' ',' >> $2
+done <<< "$roles"
+
+echo "CSV file generated: ia $2"
+
